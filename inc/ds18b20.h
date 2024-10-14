b9f4a91c7d98e87c6570c0c9243c857047c2f49c
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-10-14 14:24:55 -0300

    Removi a configuração do tick, ela não é mais necessária

--- inc/ds18b20.h
+++ inc/ds18b20.h
@@ -1,5 +1,3 @@
-#define CONFIG_SYS_CLOCK_TICKS_PER_SEC 100000000 // tempo entre ticks de 10 microssegundos
-
 /**
  * @brief Obtém a leitura de temperatura do sensor. A temperatura é lida em graus Celsius e tem resolução de 0.0625.
  * 
